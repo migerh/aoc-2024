a71339078012c8c699795e8d390d4495e42a46b5
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2024-12-19 07:45:13 +0100

    Day 19 part 2, clean up part 1

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,9 +1,7 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}};
+use std::collections::HashMap;
 
+use anyhow::{Context, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
-use anyhow::{Result, Context};
-use rayon::prelude::*;
-use memoize::memoize;
 
 use crate::utils::AocError::*;
 
@@ -11,22 +9,20 @@ type Input = (Vec<String>, Vec<String>);
 
 #[aoc_generator(day19)]
 pub fn input_generator(input: &str) -> Result<Input> {
-//    let input = "r, wr, b, g, bwu, rb, gb, br
-//
-//brwrr
-//bggr
-//gbbr
-//rrbgbr
-//ubwu
-//bwurrg
-//brgr
-//bbrgwb";
-
     let mut split = input.split("\n\n");
-    let patterns = split.next().ok_or(GenericError).context("Could not find patterns")?;
-    let designs = split.next().ok_or(GenericError).context("Could not find designs")?;
+    let patterns = split
+        .next()
+        .ok_or(GenericError)
+        .context("Could not find patterns")?;
+    let designs = split
+        .next()
+        .ok_or(GenericError)
+        .context("Could not find designs")?;
 
-    let patterns = patterns.split(", ").map(|v| v.to_string()).collect::<Vec<_>>();
+    let patterns = patterns
+        .split(", ")
+        .map(|v| v.to_string())
+        .collect::<Vec<_>>();
     let designs = designs.lines().map(|v| v.to_string()).collect::<Vec<_>>();
 
     Ok((patterns, designs))
@@ -35,89 +31,24 @@ pub fn input_generator(input: &str) -> Result<Input> {
 fn find_next(patterns: &[String], design: &str, start: usize) -> Vec<String> {
     let design = design.chars().skip(start).collect::<Vec<_>>();
 
-    patterns.iter().filter_map(|p| {
-        let len = p.len();
-        let substr = design.iter().take(len).collect::<String>();
+    patterns
+        .iter()
+        .filter_map(|p| {
+            let len = p.len();
+            let substr = design.iter().take(len).collect::<String>();
 
-        if *p == substr {
-            Some(p.clone())
-        } else {
-            None
-        }
-    })
-    .collect::<Vec<_>>()
-}
-
-fn total_length(s: &[String]) -> usize {
-    s.iter().map(|v| v.len()).sum::<usize>()
-}
-
-fn is_valid(cache: &mut HashMap<String, Vec<String>>, patterns: &[String], design: &str) -> bool {
-    let mut queue = find_next(patterns, design, 0).into_iter().map(|v| vec![v]).collect::<Vec<_>>();
-    let mut solutions = vec![];
-
-    while let Some(q) = queue.pop() {
-        let start = total_length(&q);
-
-        if start > design.len() {
-            continue;
-        }
-
-        if start == design.len() {
-            solutions.push(q);
-            return true;
-        }
-
-        let mut next = find_next(patterns, design, start).into_iter().map(|v| {
-            let mut n = q.clone();
-            n.push(v);
-            n
+            if *p == substr {
+                Some(p.clone())
+            } else {
+                None
+            }
         })
-        .filter(|v| total_length(v) <= design.len())
-        .collect::<Vec<_>>();
-
-        queue.append(&mut next);
-    }
-
-    !solutions.is_empty()
-}
-
-fn find_solutions(cache: &mut HashMap<String, Vec<String>>, patterns: Vec<String>, design: String) -> Vec<String> {
-    // println!("design {}", design);
-
-    if cache.contains_key(&design) {
-        return cache.get(&design).unwrap().clone();
-    }
-
-    if design.is_empty() {
-        return vec![];
-    }
-
-    let result = find_next(&patterns, &design, 0).into_iter().filter(|prev| prev.len() <= design.len()).flat_map(|prev| {
-        // println!("intermediate {}", prev);
-        let offset = prev.len();
-        let next = design.chars().skip(offset).collect::<String>();
-        let result = find_solutions(cache, patterns.clone(), next.to_string()).iter().map(|post| {
-            prev.clone() + post
-        }).collect::<Vec<_>>();
-
-        if result.is_empty() && next.is_empty() {
-            return vec![prev];
-        }
-
-        result
-    }).collect::<Vec<_>>();
-
-    cache.entry(design).or_insert(result.clone());
-
-    // println!("found {:?}", result);
-
-    result
+        .collect::<Vec<_>>()
 }
 
 fn find_validity(cache: &mut HashMap<String, bool>, patterns: Vec<String>, design: String) -> bool {
     if cache.contains_key(&design) {
-        return cache.get(&design).unwrap().clone();
+        return *cache.get(&design).unwrap();
     }
 
     if design.is_empty() {
@@ -144,28 +75,82 @@ fn find_validity(cache: &mut HashMap<String, bool>, patterns: Vec<String>, desig
 #[aoc(day19, part1)]
 pub fn solve_part1(input: &Input) -> Result<usize> {
     let (patterns, designs) = input;
-    println!("# patterns {}, # designs: {}", patterns.len(), designs.len());
-
     let mut cache = HashMap::new();
+    let result = designs
+        .iter()
+        .filter(|d| find_validity(&mut cache, patterns.clone(), d.to_string()))
+        .collect::<Vec<_>>();
 
-    // let result = designs.iter().filter(|d| is_valid(&mut cache, patterns, d)).count();
-    let result = designs.iter().enumerate().filter(|d| {
-        let result = find_validity(&mut cache, patterns.clone(), d.1.to_string());
-
-        println!("# {}, result {}", d.0, result);
-
-        result
-    }).collect::<Vec<_>>();
-    println!("{:?}", result);
     Ok(result.len())
 }
 
+fn find_number(cache: &mut HashMap<String, usize>, patterns: Vec<String>, design: String) -> usize {
+    if cache.contains_key(&design) {
+        return *cache.get(&design).unwrap();
+    }
+
+    if design.is_empty() {
+        return 0;
+    }
+
+    let next = find_next(&patterns, &design, 0)
+        .into_iter()
+        .map(|n| {
+            let offset = n.len();
+            let next = design.chars().skip(offset).collect::<String>();
+            let result = find_number(cache, patterns.clone(), next.to_string());
+
+            if result == 0 && next.is_empty() {
+                return 1;
+            }
+
+            result
+        })
+        .sum();
+
+    cache.entry(design).or_insert(next);
+
+    next
+}
+
 #[aoc(day19, part2)]
-pub fn solve_part2(input: &Input) -> Result<i32> {
-    Ok(0)
+pub fn solve_part2(input: &Input) -> Result<usize> {
+    let (patterns, designs) = input;
+    let mut cache = HashMap::new();
+    let result = designs
+        .iter()
+        .map(|d| find_number(&mut cache, patterns.clone(), d.to_string()))
+        .sum::<usize>();
+
+    Ok(result)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    fn input() -> &'static str {
+        "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"
+    }
+
+    #[test]
+    fn part1() -> Result<()> {
+        let data = input_generator(input())?;
+        Ok(assert_eq!(6, solve_part1(&data)?))
+    }
+
+    #[test]
+    fn part2() -> Result<()> {
+        let data = input_generator(input())?;
+        Ok(assert_eq!(16, solve_part2(&data)?))
+    }
 }
