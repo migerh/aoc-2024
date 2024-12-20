13d4d8f098e4cd6bca5e7911242d56834a90eddd
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2024-12-20 07:51:59 +0100

    Day 20, slow but works
    Takes ~5s for part 1, ~15s for part 2

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::{Context, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
-use pathfinding::prelude::dijkstra;
+use pathfinding::prelude::{dijkstra, dijkstra_all};
 use rayon::prelude::*;
 
 use crate::utils::AocError::*;
@@ -31,21 +31,21 @@ impl Cheat {
 
 #[aoc_generator(day20)]
 pub fn input_generator(input: &str) -> Result<PlotMap> {
-    let input = "###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############";
+//    let input = "###############
+//#...#...#.....#
+//#.#.#.#.#.###.#
+//#S#...#.#.#...#
+//#######.#.#.###
+//#######.#.#...#
+//#######.#.###.#
+//###..E#...#...#
+//###.#######.###
+//#...###...#...#
+//#.#####.#.###.#
+//#.#...#.#.#...#
+//#.#.#.#.#.#.###
+//#...#...#...###
+//###############";
 
     let map = input
         .lines()
@@ -59,7 +59,7 @@ pub fn input_generator(input: &str) -> Result<PlotMap> {
     Ok(map)
 }
 
-fn successors(map: &PlotMap, pos: &Coords) -> Vec<(Coords, u32)> {
+fn successors(map: &PlotMap, pos: &Coords, cheat: bool) -> Vec<(Coords, u32)> {
     let dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
     dirs.into_iter()
@@ -67,7 +67,7 @@ fn successors(map: &PlotMap, pos: &Coords) -> Vec<(Coords, u32)> {
             let c = (pos.0 + d.0, pos.1 + d.1);
             let v = map.get(&c)?;
 
-            if *v != '#' {
+            if (!cheat && *v != '#') || (cheat && *v == '#') {
                 Some(((c.0, c.1), 1))
             } else {
                 None
@@ -113,7 +113,7 @@ fn identify_possible_cheats(map: &PlotMap, race_track: &[Coords]) -> Result<Vec<
                                     } else {
                                         let path = dijkstra(
                                             &new_start,
-                                            |n| successors(map, n),
+                                            |n| successors(map, n, false),
                                             |n| end.0 == n.0 && end.1 == n.1,
                                         )?;
 
@@ -147,7 +147,7 @@ pub fn solve_part1(input: &PlotMap) -> Result<u32> {
     let end = find_node(input, &'E')?;
     let path = dijkstra(
         &start,
-        |n| successors(input, n),
+        |n| successors(input, n, false),
         |n| end.0 == n.0 && end.1 == n.1,
     )
     .ok_or(GenericError)
@@ -175,13 +175,112 @@ pub fn solve_part1(input: &PlotMap) -> Result<u32> {
     Ok(result)
 }
 
+fn distance(start: &Coords, end: &Coords) -> u32 {
+    ((end.0 - start.0).abs() + (end.1 - start.1).abs()) as u32
+}
+
+fn get_shortest_path_length_cached(cache: &mut HashMap<Coords, u32>, map: &PlotMap, start: &Coords, end: &Coords) -> Option<u32> {
+    if cache.contains_key(start) {
+        return cache.get(&start).copied();
+    }
+
+    let path = dijkstra(
+        start,
+        |n| successors(map, n, false),
+        |n| end.0 == n.0 && end.1 == n.1,
+    )?;
+    cache.entry(*start).or_insert(path.1);
+
+    Some(path.1.clone())
+}
+
+fn identify_cheats_part2(map: &PlotMap, race_track: &[Coords]) -> Result<Vec<Cheat>> {
+    let end = find_node(map, &'E')?;
+    let uncheated_result = race_track.len() as u32 - 1;
+    let mut cache = HashMap::new();
+
+    let cheats = race_track
+        .iter()
+        .enumerate()
+        .flat_map(|(current_len, p)| {
+            // simple flood fill into the wall
+            // doesn't work, i don't know the min path through the wall :/
+            /*
+            let mut queue = dirs
+                .iter()
+                .map(|d| (p.0 + d.0, p.1 + d.1))
+                .filter(|p| {
+                    if let Some(c) = map.get(&p) {
+                        *c == '#'
+                    } else {
+                        false
+                    }
+                })
+                .map(|p| (p, 1_u32))
+                .collect::<Vec<_>>();
+
+            let mut new_starts = HashSet::new();
+            let mut visited = HashSet::new();
+            while let Some(q) = queue.pop() {
+                let (v, t) = q;
+
+                if t >= 20 || visited.contains(&v) {
+                    continue;
+                }
+                visited.insert(v);
+
+                for c in dirs.iter().map(|d| (v.0 + d.0, v.1 + d.1)) {
+                    if let Some(w) = map.get(&c) {
+                        if *w == '#' {
+                            queue.push((c, t + 1));
+                        }
+
+                        if *w != '#' && c.0 != p.0 && c.1 != p.1 {
+                            new_starts.insert((c, );
+                        }
+                    }
+                }
+            }*/
+
+
+
+            let new_starts = map.iter().filter_map(|(k, c)| {
+                let d = distance(p, k);
+                if *c != '#' && d <= 20 {
+                    Some((k, d))
+                } else {
+                    None
+                }
+            }).collect::<Vec<_>>();
+
+            new_starts.iter().filter_map(|new_start| {
+                let cheated_path_rest = get_shortest_path_length_cached(&mut cache, map, new_start.0, &end)?;
+
+                let new_race_length = current_len as u32 + cheated_path_rest + new_start.1;
+                if uncheated_result > new_race_length {
+                    Some(Cheat::new(
+                        *p,
+                        *new_start.0,
+                        uncheated_result - new_race_length,
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    Ok(cheats)
+}
+
 #[aoc(day20, part2)]
 pub fn solve_part2(input: &PlotMap) -> Result<i32> {
     let start = find_node(input, &'S')?;
     let end = find_node(input, &'E')?;
     let path = dijkstra(
         &start,
-        |n| successors(input, n),
+        |n| successors(input, n, false),
         |n| end.0 == n.0 && end.1 == n.1,
     )
     .ok_or(GenericError)
@@ -190,7 +289,7 @@ pub fn solve_part2(input: &PlotMap) -> Result<i32> {
     println!("{:?}", path);
     println!("len {}", path.0.len());
 
-    let cheats = identify_possible_cheats(input, &path.0)?;
+    let cheats = identify_cheats_part2(input, &path.0)?;
     println!("{:?}", cheats);
 
     let mut stats_map = HashMap::new();
@@ -201,6 +300,13 @@ pub fn solve_part2(input: &PlotMap) -> Result<i32> {
             .or_insert(1);
     }
     println!("{:?}", stats_map);
+
+    //let mut example = stats_map.iter().filter(|v| **v.0 >= 50).collect::<Vec<_>>();
+    //example.sort_by(|a, b| a.0.cmp(b.0));
+
+    //for e in example {
+    //    println!("{} cheats that save {} ps", e.1, e.0);
+    //}
 
     let result = stats_map
         .iter()
